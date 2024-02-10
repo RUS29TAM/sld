34fc0e032652c49e939f946175402214695fbe3c
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-02-10 23:25:07 +0300

    chore: add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const nextDom = document.getElementById('next');
+export const prevDom = document.getElementById('prev');
+export const carouselDom = document.querySelector('.carousel');
+export const listItemDom = document.querySelector('.carousel .list');
+export const thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+export const itemSlider = document.querySelectorAll('.carousel .list .item');
+export const itemThumbnail = document.querySelectorAll('.carousel .thumbnail');
