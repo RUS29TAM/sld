f3f4b6d82567e410824283a37d8a0d7ace2d70df
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-02-15 17:16:40 +0300

    feat: create component loading-lane

---
+++ src/app/components/loading-lane/loading-lane.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from "./loading-lane.module.css";
+
+const LoadingLane = () => {
+    return (
+        <div className={styles.time}></div>
+    );
+};
+export default LoadingLane;
