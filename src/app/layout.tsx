796595db6e0ac68ea7486111a7420fbfc4f5b728
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-02-19 14:25:52 +0300

    fix: 320x568 display & selector names & remove prevent automatic screen rotation on touchscreen devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
     return (
         <>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             </Head>
             <html lang="en">
                 <body className={inter.className}>
