145e6feacb8b924045fe5f0d7c7f582a9dcf7d7f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-02-24 20:27:31 +0300

    chore: add theme switching functionality

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -1,15 +1,34 @@
+'use client'
+
 import React from 'react';
 import styles from './header.module.css'
 import Link from "next/link";
+import {useTheme} from "../../ThemeContext";
 
 const Header = () => {
+    const { toggleTheme, isDarkTheme  } = useTheme();
+
+    const handleButtonClick = () => {
+        toggleTheme();
+        document.querySelector("#toggle-theme")?.parentElement?.classList.toggle("checked");
+    };
     return (
         <header className={styles.header}>
             <nav>
                 <Link href={'/pages/main'}>ГЛАВНАЯ</Link>
                 <Link href={'/pages/contact'}>КОНТАКТЫ</Link>
                 <Link href={'/pages/info'}>ИНФО</Link>
+                <Link href={'/pages/draft'}>draft</Link>
+
             </nav>
+            <input onChange={handleButtonClick} className={styles.input} type="checkbox" id="switch"/>
+            <label className={styles.label} htmlFor="switch">
+                {isDarkTheme
+                    ?
+                    <span className={styles.spanLight}>Light</span>
+                    :
+                    <span className={styles.spanNight}>Night</span>}
+            </label>
         </header>
     );
 };
