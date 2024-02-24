48390bfe30922c565ab68b4d49dfbcef44d26c22
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-02-24 20:26:35 +0300

    chore: wrap components it in a themeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import {Inter} from "next/font/google";
 import "./styles/globals.css";
 import Header from "@/app/components/header/page";
 import Footer from "@/app/components/footer/footer";
+import {ThemeProvider} from "./ThemeContext";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -15,16 +16,20 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            </Head>
-            <html lang="en">
+            <ThemeProvider>
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                </Head>
+                <html lang="en">
                 <body className={inter.className}>
-                    <Header/>
-                    {children}
-                    <Footer />
+                <Header/>
+                {children}
+                <Footer />
                 </body>
-            </html>
+                </html>
+
+            </ThemeProvider>
+
         </>
     );
 }
