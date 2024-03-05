bf39c99e2c95eb102ddb3c677ddac4af9833a4ab
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-03-05 23:56:14 +0300

    chore: draft data for two page

---
+++ src/utils/blocksData.ts
@@ -0,0 +1,53 @@
+import { StaticImageData } from 'next/image';
+
+export const blocksData: { id: string, title: string, leftContent: string, rightContent: string, num: number}[] = [
+    {
+        id: "Purple",
+        title: "Purple",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 1,
+    },
+    {
+        id: "Green",
+        title: "Green",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 2,
+    },    {
+        id: "Chocolate",
+        title: "Chocolate",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 3,
+    },    {
+        id: "Blue",
+        title: "Blue",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 4,
+    },    {
+        id: "Yellow",
+        title: "Yellow",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 5,
+    },    {
+        id: "Red",
+        title: "Red",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 6,
+    },    {
+        id: "Pink",
+        title: "Pink",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 7,
+    },    {
+        id: "Orange",
+        title: "Orange",
+        leftContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+        rightContent: "Nunc aliquet bibendum enim facilisis gravida neque convallis a...",
+        num: 8,
+    },];
