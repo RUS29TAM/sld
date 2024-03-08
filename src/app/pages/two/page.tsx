a94ffc49a947eb342e7181a0a362940a7fd22e7f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-03-08 23:00:05 +0300

    chore: draft two page

--- src/app/pages/two/page.tsx
+++ src/app/pages/two/page.tsx
@@ -1,8 +1,7 @@
-
 import React from 'react';
+import styles from './two.module.css';
 import Block from '../../components/block/block';
 import { blocksData } from '@/utils/blocksData';
-import './two.css'
 import LoadingLane from "@/app/components/loading-lane/loading-lane";
 const Page = () => {
     return (
@@ -10,33 +9,33 @@ const Page = () => {
         <>
             <LoadingLane/>
 
-            <div className="box">
+            <div className={styles.box}>
 
-                <div className="navigation">
-                    <ol>
-                        {blocksData.map((block) => (
-                            <li key={block.id}>
-                                <a href={`#${block.id}`}>{block.id}</a>
+                <div className={styles.navigation}>
+                    <ol className={styles.ol}>
+                        {blocksData.map((blocksData) => (
+                            <li className={styles.li} key={blocksData.id}>
+                                <a className={styles.a} href={`#${blocksData.id}`}>{blocksData.id}</a>
                             </li>
                         ))}
                     </ol>
                 </div>
 
-                <article className="article">
-                    {blocksData.map((block, index) => (
-                        <Block key={index} {...block} />
+                <article className={styles.article}>
+                    {blocksData.map((blocksData, index) => (
+                        <Block key={index} {...blocksData} />
                     ))}
                 </article>
 
-                <div className="preloader">
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                <div className={styles.preloader}>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
                 </div>
             </div>
         </>
