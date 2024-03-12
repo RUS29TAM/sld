d2cf9a157a82710fa219d14351520b431d41dc67
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-03-12 18:00:37 +0300

    chore: add flip-cards component

--- src/app/pages/social-contract/page.tsx
+++ src/app/pages/social-contract/page.tsx
@@ -1,58 +1,51 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 import { useTheme } from '../../ThemeContext';
 
 import styles from './social-contract.module.css'
 import LoadingLane from "@/app/components/loading-lane/loading-lane";
+import FlipCard from "@/app/components/flip-card/flip-card";
 
-
+const cardData = [
+    { title: 'Поиск работы', frontContent: 'Поиск работы', backContent: 'Выплаты при заключении контракта и три месяца после трудоустройства, стипендия на период обучения, оплачиваемая стажировка' },
+    { title: 'Открытие бизнеса', frontContent: 'Открытие бизнеса', backContent: 'До 350 000 руб.' },
+    { title: 'Развитие подсобного хозяйства', frontContent: 'Развитие подсобного хозяйства', backContent: 'До 200 000 руб.' },
+    { title: 'Обучение или переобучение', frontContent: 'Обучение или переобучение', backContent: 'До 30 000 руб. на получение навыков предпринимательской деятельности или ведения хозяйства, на освоение новой профессии' },
+    { title: 'Преодоление сложной жизненной ситуации', frontContent: 'Преодоление сложной жизненной ситуации', backContent: 'Единоразовая или ежемесячная финансовая помощь' },
+];
 const SocialContract = () => {
     const { isDarkTheme } = useTheme();
+    const [cardStates, setCardStates] = useState([false, false, false, false, false]);
 
+    const handleCardClick = (index: number) => {
+        const updatedCardStates = cardStates.map((state, i) => (i === index ? !state : false));
+        setCardStates(updatedCardStates);
+    };
     return (
         <>
-            <div className={`${styles.container} ${isDarkTheme ? styles.darkTheme : styles.lightTheme }`}>
+            <div className={`${styles.container} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
                 <LoadingLane/>
                 <h1 className={styles.title}>Цели социального контракта и размеры выплат</h1>
-                <div className={styles.subtitle}>
-                    <h2 className={styles.h2}>Государство предоставляет социальную помощь в рамках социального контракта
-                        по разным
-                        направлениям, и каждое направление имеет свои условия финансовой поддержки. Если, например,
-                        сумма для обучения составляет не более 30 000 рублей, то для открытия собственного бизнеса
-                        предоставляется возможность получить единовременное пособие, которое может достигать 350 000
-                        рублей.</h2>
-                    <div className={styles.table}>
-                        <p>1</p>
-                        <p>Поиск работы</p>
-                        <p>Размер выплат</p>
-                        <p>Выплаты при заключении контракта и три месяца после трудоустройства, стипендия на период
-                            обучения, оплачиваемая стажировка
-                        </p>
-                        <p>2</p>
-                        <p>Открытие бизнеса </p>
-                        <p>Размер выплат</p>
-                        <p>До 350 000 руб.
-                        </p>
-                        <p>3</p>
-                        <p>Развитие подсобного хозяйства</p>
-                        <p>Размер выплат</p>
-                        <p>До 200 000 руб.
-                        </p>
-                        <p>4</p>
-                        <p>Обучение или переобучение</p>
-                        <p>Размер выплат</p>
-                        <p>До 30 000 руб. на получение навыков предпринимательской деятельности или ведения хозяйства,
-                            на освоение новой профессии</p>
-                        <p>5</p>
-                        <p>Обучение или переобучение</p>
-                        <p>Размер выплат</p>
-                        <p>Единоразовая или ежемесячная финансовая помощь
-                        </p>
-                    </div>
+                <h2 className={styles.subtitle}>Государство предоставляет социальную помощь в рамках социального контракта
+                    по разным
+                    направлениям, и каждое направление имеет свои условия финансовой поддержки. Если, например,
+                    сумма для обучения составляет не более 30 000 рублей, то для открытия собственного бизнеса
+                    предоставляется возможность получить единовременное пособие, которое может достигать 350 000
+                    рублей.</h2>
+                <div className={styles.cardContainer}>
+                    {cardData.map((data, index) => (
+                        <FlipCard
+                            key={index}
+                            isOpen={cardStates[index]}
+                            onClick={() => handleCardClick(index)}
+                            title={data.title}
+                            frontContent={data.frontContent}
+                            backContent={data.backContent}
+                        />
+                    ))}
                 </div>
             </div>
         </>
-
     );
 };
 export default SocialContract;
