4e61589a4b5d9db07f80145d18213bf8a094b4be
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-04-12 16:49:35 +0300

    chore: no change, nothing significant

--- src/app/pages/two/page.tsx
+++ src/app/pages/two/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styles from './two.module.css';
 import Block from '../../components/block/block';
-import { blocksData } from '@/utils/blocksData';
+import {blocksData} from '@/utils/blocksData';
 import LoadingLane from "@/app/components/loading-lane/loading-lane";
+
 const Page = () => {
     return (
-
         <>
             <LoadingLane/>
-
             <div className={styles.box}>
-
                 <div className={styles.navigation}>
                     <ol className={styles.ol}>
                         {blocksData.map((blocksData) => (
@@ -28,18 +26,33 @@ const Page = () => {
                 </article>
 
                 <div className={styles.preloader}>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
-                    <img className={styles.img} src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
+                    <img className={styles.img}
+                         src="https://images.unsplash.com/photo-1682687982049-b3d433368cd1?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                         width="1920" height="1080" alt="preload"/>
                 </div>
             </div>
         </>
-
     );
 };
 
