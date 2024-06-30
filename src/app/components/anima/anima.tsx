c814811f7551c0f477e509110d698087a715f8cc
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-06-30 23:44:39 +0300

    chore: add component anima

---
+++ src/app/components/anima/anima.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './anima.css'
+const Anima = () => {
+    return (
+        <div className="content">
+            <div className="cube"></div>
+        </div>
+    );
+};
+
+export default Anima;
