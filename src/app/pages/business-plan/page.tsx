3e9810812c4a925596b9a75f8a70f7c94f884a76
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-06-30 23:43:03 +0300

    chore: add page business-plan

---
+++ src/app/pages/business-plan/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Anima from "@/app/components/anima/anima";
+import styles from './business-plan.module.css'
+const Page = () => {
+    return (
+        <div className={styles.div} >
+            <Anima />
+        </div>
+    );
+};
+
+export default Page;
