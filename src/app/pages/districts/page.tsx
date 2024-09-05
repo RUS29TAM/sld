8672b7ace51ebf75bdc049823255b4f12f34fe6f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-05 17:58:34 +0300

    chore: add page District

---
+++ src/app/pages/districts/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Map from '../../components/map/map'
+
+const Page = () => {
+    return (
+        <div>
+            <Map />
+        </div>
+    );
+};
+
+export default Page;
