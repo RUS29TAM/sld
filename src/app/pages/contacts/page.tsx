5ceb9f1a1aa574b3b2c28cd6464e36fe26a186d9
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-05 17:58:01 +0300

    chore: add template for contacts page

---
+++ src/app/pages/contacts/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Accordion from "@/app/components/accordion/accordion";
+
+const Page = () => {
+    return (
+        <div>
+            <h1 style={{textAlign: "center", color: '#fff'}}>Контакты</h1>
+        </div>
+    );
+};
+
+export default Page;
