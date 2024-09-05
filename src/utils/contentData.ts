7bfc190c4067d7f613c5aef4497f8a312d468308
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-05 17:52:07 +0300

    chore: add contentData

---
+++ src/utils/contentData.ts
@@ -0,0 +1,35 @@
+// Массив данных, который будет использоваться для отображения контента
+export const contentData = [
+    {
+        title: "СМСП",
+        content: "This is the content for Card 1. Here you can describe more details."
+    },
+    {
+        title: "Франшизы",
+        content: "This is the content for Card 2. Here you can describe more details."
+    },
+    {
+        title: "Card 3",
+        content: "This is the content for Card 3. Here you can describe more details."
+    },
+    {
+        title: "Card 4",
+        content: "This is the content for Card 4. Here you can describe more details."
+    },
+    {
+        title: "Card 5",
+        content: "This is the content for Card 5. Here you can describe more details."
+    },
+    {
+        title: "Card 6",
+        content: "This is the content for Card 6. Here you can describe more details."
+    },
+    {
+        title: "Card 7",
+        content: "This is the content for Card 7. Here you can describe more details."
+    },
+    {
+        title: "Card 8",
+        content: "This is the content for Card 8. Here you can describe more details."
+    }
+];
