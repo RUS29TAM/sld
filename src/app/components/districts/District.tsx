9da1aeac2b89df28a33a9fdcbcff7c08ab29301f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-10 14:56:52 +0300

    chore: configure component

--- src/app/components/districts/District.tsx
+++ src/app/components/districts/District.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './district.module.css'
 import CardsGrid from "@/app/components/cardsGrid/CardsGrid";
+import {contentData} from "@/utils/contentData";
+import EconomyQuestions from "@/app/components/questions/economy-questions/economy-questions";
 
 const District = () => {
     return (
         <div className={styles.wrapper}>
-            <h1 className={styles.title}>Ленский район</h1>
-            <CardsGrid />
+            <EconomyQuestions />
         </div>
     );
 };
