8568fecee33c77fb5d272de2f04034c565432915
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-10 14:56:29 +0300

    chore: configure contentData

--- src/utils/contentData.ts
+++ src/utils/contentData.ts
@@ -1,34 +1,34 @@
 // Массив данных, который будет использоваться для отображения контента
 export const contentData = [
-    {   district: "Ленский район",
-        title: "СМСП",
+    {
+        title: "Card 1",
         content: "This is the content for Card 1. Here you can describe more details."
     },
-    {   district: "Ленский район",
-        title: "Франшизы",
+    {
+        title: "Card 2",
         content: "This is the content for Card 2. Here you can describe more details."
     },
-    {   district: "Ленский район",
+    {
         title: "Card 3",
         content: "This is the content for Card 3. Here you can describe more details."
     },
-    {   district: "Ленский район",
+    {
         title: "Card 4",
         content: "This is the content for Card 4. Here you can describe more details."
     },
-    {   district: "Ленский район",
+    {
         title: "Card 5",
         content: "This is the content for Card 5. Here you can describe more details."
     },
-    {   district: "Ленский район",
+    {
         title: "Card 6",
         content: "This is the content for Card 6. Here you can describe more details."
     },
-    {   district: "Ленский район",
+    {
         title: "Card 7",
         content: "This is the content for Card 7. Here you can describe more details."
     },
-    {   district: "Ленский район",
+    {
         title: "Card 8",
         content: "This is the content for Card 8. Here you can describe more details."
     }
