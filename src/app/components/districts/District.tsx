5d2a8a3261871cec689279b72c656716a8c41bc7
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-10 15:02:45 +0300

    chore: configure component

--- src/app/components/districts/District.tsx
+++ src/app/components/districts/District.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import styles from './district.module.css'
-import CardsGrid from "@/app/components/cardsGrid/CardsGrid";
-import {contentData} from "@/utils/contentData";
-import EconomyQuestions from "@/app/components/questions/economy-questions/economy-questions";
 
 const District = () => {
     return (
         <div className={styles.wrapper}>
-            <EconomyQuestions />
+
         </div>
     );
 };
