c1349bad654f2e81881d8b63790d0d4d2ae0d51f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-11 16:19:25 +0300

    chore: customize the selection of data from the object array depending on the context & a pop-up window appears when you click on a block

--- src/app/pages/franchise/page.tsx
+++ src/app/pages/franchise/page.tsx
@@ -1,34 +1,41 @@
 'use client'
-import React, {useState} from 'react';
+import React, {ReactNode, useState} from 'react';
 import styles from './franchise.module.css'
 import Modal from '@/app/components/modal/modal';
 import {MdCurrencyRuble, MdOutlineStarBorderPurple500, MdSupportAgent} from 'react-icons/md';
 import {GoLaw} from 'react-icons/go';
 import {FaRegHandshake} from 'react-icons/fa6';
 import Link from "next/link";
+import {useTheme} from "@/app/ThemeContext";
 
 const Page = () => {
+    const {setSelectedTopic} = useTheme();
     const [openPopup, setOpenPopup] = useState(false);
-    const [popupContent, setPopupContent] = useState(false);
+    const [popupContent, setPopupContent] = useState<string | null | ReactNode>(null);
 
-    const handleClickNum = (content: any) => {
+    const handleClickNum = (content: ReactNode, topic: string) => {
         setPopupContent(content);
         setOpenPopup(true)
+        setSelectedTopic(topic);  // Сохраняем выбранную тему в контексте
     };
 
     return (
         <div className={styles.container}>
             <div className={styles.timelineContainer}>
-                {/*Individual timeline point*/}
-                <div className={styles.timelinePoint}>
-                    <i className={`${styles.faSolid} ${styles.faIcons}`}>
-                        <div onClick={() => handleClickNum(<div className={styles.description}><p>Помогут Вам оценить
-                            качественные и количественные характеристики франчайзера, риски работы с
-                            данной компанией, увидеть, насколько она серьезно подходит к построению сети и выбору
-                            партнеров.</p> <Link className={styles.link} href={'/pages/solution-selection'}>Перейти к
-                            ознакомлению</Link></div>)} className={styles.faIcons}>1
-                        </div>
-                    </i>
+                {/*general timeline point*/}
+                <div className={styles.timelinePoint}
+                     onClick={() => handleClickNum(
+                         <div className={styles.description}>
+                             <p>
+                                 Вам будет предоставлена возможность оценить как качественные, так и количественные
+                                 аспекты франчайзера, выявить риски сотрудничества с данной компанией и оценить,
+                                 насколько она серьезно подходит к развитию сети и выбору партнеров.
+                             </p>
+                             <Link className={styles.link} href={'/pages/questions-list'}>
+                                 Перейти к ознакомлению
+                             </Link>
+                         </div>, ('general'))}>
+                    <i className={`${styles.faSolid} ${styles.faIcons}`}>1</i>
                     <div className={styles.popup}>
                         <div className={styles.popupNumber}><MdOutlineStarBorderPurple500/></div>
                         <div className={styles.popupDetails}>
@@ -37,20 +44,21 @@ const Page = () => {
                     </div>
                 </div>
 
-                {/*Individual timeline point*/}
-                <div className={styles.timelinePoint}>
-                    <i className={`${styles.faSolid} ${styles.faCopyright}`}>
-                        <div onClick={() => handleClickNum(<div className={styles.description}><p>Экономический
-                            блок; вопросов поможет Вам оценить собственные ресурсы, грамотно
-                            спланировать
-                            бюджет открытия, рассчитать прибыль и необходимые дополнительные вливания. Кроме того, часто
-                            франчайзеры занижают объем необходимых в открытие средств или не афишируют те или иные
-                            платежи. Наша задача - узнать обо всем до принятия решения.
-                            партнеров.</p>
-                            <button>Далее</button>
-                        </div>)} className={styles.faIcons}>2
-                        </div>
-                    </i>
+                {/*economy timeline point*/}
+                <div className={styles.timelinePoint}
+                     onClick={() => handleClickNum(
+                         <div className={styles.description}>
+                             <p>Блок &quot;Экономических&quot; вопросов поможет вам оценить ваши собственные ресурсы,
+                                 тщательно
+                                 спланировать бюджет на открытие, рассчитать ожидаемую прибыль и предвидеть необходимые
+                                 дополнительные инвестиции. Часто бывает, что франчайзеры занижают общую сумму,
+                                 необходимую для старта, или утаивают определенные платежи. Наша цель - узнать обо всем
+                                 этом до принятия решения.</p>
+                             <Link className={styles.link} href={'/pages/questions-list'}>
+                                 Перейти к ознакомлению
+                             </Link>
+                         </div>, ('economy'))}>
+                    <i className={`${styles.faSolid} ${styles.faIcons}`}>2</i>
                     <div className={styles.popup}>
                         <div className={styles.popupNumber}><MdCurrencyRuble/></div>
                         <div className={styles.popupDetails}>
@@ -59,19 +67,24 @@ const Page = () => {
                     </div>
                 </div>
 
-                {/*Individual timeline point */}
-                <div className={styles.timelinePoint}>
-                    <i className={`${styles.faSolid} ${styles.faBullseye}`}>
-                        <div onClick={() => handleClickNum(<div className={styles.description}><p>Время переговоров на
-                            словах франчайзер может много всего обещать, но обращать внимание
-                            всегда стоит на договор и его пункты. Чем более подробный договор Вы заключаете, тем меньше
-                            вероятность внезапных решений франчайзера и негативных последствий. В идеале договор
-                            франчайзинга состоит не менее чем из 20 листов и содержит подробную информацию о каждом
-                            этапе сотрудничества и обязательствах сторон по всем аспектам.</p>
-                            <button>Далее</button>
-                        </div>)} className={styles.faIcons}>3
-                        </div>
-                    </i>
+                {/*law timeline point */}
+                <div className={styles.timelinePoint}
+                     onClick={() => handleClickNum(
+                         <div className={styles.description}>
+                             <p>
+                                 В процессе переговоров франчайзер может высказывать различные обещания, но ключевое
+                                 внимание следует уделить содержанию договора и его положениям. Чем более
+                                 детализированным будет договор, тем меньше шансов на внезапные действия франчайзера и
+                                 негативные последствия. Идеальный договор франчайзинга составляется обширно, содержа
+                                 подробную информацию о каждом этапе сотрудничества и обязанностях сторон во всех
+                                 аспектах. Обычно такой договор состоит из не менее чем 20 страниц, отражая все аспекты
+                                 сотрудничества.
+                             </p>
+                             <Link className={styles.link} href={'/pages/questions-list'}>
+                                 Перейти к ознакомлению
+                             </Link>
+                         </div>, ('law'))}>
+                    <i className={`${styles.faSolid} ${styles.faIcons}`}>3</i>
                     <div className={styles.popup}>
                         <div className={styles.popupNumber}><GoLaw/></div>
                         <div className={styles.popupDetails}>
@@ -80,16 +93,19 @@ const Page = () => {
                     </div>
                 </div>
 
-                {/*Individual timeline point*/}
-                <div className={styles.timelinePoint}>
-                    <i className={`${styles.faSolid} ${styles.faCompass}`}>
-                        <div onClick={() => handleClickNum(<div className={styles.description}><p>То, как франчайзер
-                            отвечает на Ваши вопросы, показывает его отношение к своим партнерам и
-                            готовность помогать на стадии запуска Вашего бизнеса</p>
-                            <button>Далее</button>
-                        </div>)} className={styles.faIcons}>4
-                        </div>
-                    </i>
+                {/*support timeline point*/}
+                <div className={styles.timelinePoint}
+                     onClick={() => handleClickNum(
+                         <div className={styles.description}>
+                             <p>
+                                 Способ, которым франчайзер реагирует на ваши запросы, отражает его отношение к
+                                 партнерам и готовность помогать в начальной стадии вашего бизнеса.
+                             </p>
+                             <Link className={styles.link} href={'/pages/questions-list'}>
+                                 Перейти к ознакомлению
+                             </Link>
+                         </div>, ('support'))}>
+                    <i className={`${styles.faSolid} ${styles.faIcons}`}>4</i>
                     <div className={styles.popup}>
                         <div className={styles.popupNumber}><MdSupportAgent/></div>
                         <div className={styles.popupDetails}>
@@ -98,21 +114,22 @@ const Page = () => {
                     </div>
                 </div>
 
-                {/*Individual timeline point*/}
-                <div className={styles.timelinePoint}>
-                    <i className={`${styles.faSolid} ${styles.faFaceGrinBeamSweat}`}>
-                        <div
-                            onClick={() => handleClickNum(<div className={styles.description}><p>Необходимо быть готовым
-                                к любым жизненным ситуациям и понимать, на что франчайзер имеет
-                                право в рамках договора, а в чем Вы ему можете отказать. И наоборот, что франчайзер
-                                может
-                                контролировать в Вашем бизнесе и за что он может Вас штрафовать и наказывать, а за что
-                                может
-                                даже отозвать франшизу.</p>
-                                <button>Далее</button>
-                            </div>)} className={styles.faIcons}>5
-                        </div>
-                    </i>
+                {/*partnership timeline point*/}
+                <div className={styles.timelinePoint}
+                     onClick={() => handleClickNum(
+                         <div className={styles.description}>
+                             <p>
+                                 Важно быть подготовленным к различным сценариям и понимать границы в рамках соглашения
+                                 с франчайзером. Необходимо осознавать, на что у него есть право согласно договору, а
+                                 что может быть отклонено. Также важно осознавать, какие аспекты бизнеса контролирует
+                                 франчайзер и за какие нарушения он может наложить штрафы или даже прекратить
+                                 сотрудничество по франшизе.
+                             </p>
+                             <Link className={styles.link} href={'/pages/questions-list'}>
+                                 Перейти к ознакомлению
+                             </Link>
+                         </div>, ('partnership'))}>
+                    <i className={`${styles.faSolid} ${styles.faIcons}`}>5</i>
                     <div className={styles.popup}>
                         <div className={styles.popupNumber}><FaRegHandshake/></div>
                         <div className={styles.popupDetails}>
