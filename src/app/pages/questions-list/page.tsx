06e5654a4de487603cd91662c1881634807641d4
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-11 16:17:25 +0300

    chore: customize the selection of data from the object array depending on the context

--- src/app/pages/questions-list/page.tsx
+++ src/app/pages/questions-list/page.tsx
@@ -1,22 +1,44 @@
 'use client'
-import React, {useEffect, useRef, useState} from 'react';
-import { questionsData, Question  } from './EconomyQuestionsText';
+import React, {useEffect, useState} from 'react';
+import { economyQuestionsData, generalQuestionsData, lawQuestionData, supportQuestionsData, partnershipQuestionData, Question  } from '../../../utils/questions-list-data';
 import styles from './questions-list.module.css';
 import Link from "next/link";
+import {useTheme} from "@/app/ThemeContext";
 
-const QuestionsList: React.FC = () => {
-    const [activeQuestion, setActiveQuestion] = useState<Question  | null>(null);
+const Page: React.FC = () => {
+    const { selectedTopic } = useTheme(); // Получаем выбранную тему из контекста
+
+    const [questionsData, setQuestionsData] = useState<Question[]>(economyQuestionsData); // default data
+    const [activeQuestion, setActiveQuestion] = useState<Question | null>(null);
     const [isCommentVisible, setIsCommentVisible] = useState(false);
+
+    // Загружаем данные на основе выбранной темы
+    useEffect(() => {
+        if (selectedTopic === 'general') {
+            setQuestionsData(generalQuestionsData);
+        } else if (selectedTopic === 'economy') {
+            setQuestionsData(economyQuestionsData);
+        } else if (selectedTopic === 'law') {
+            setQuestionsData(lawQuestionData);
+        } else if (selectedTopic === 'support') {
+            setQuestionsData(supportQuestionsData);
+        } else if (selectedTopic === 'partnership') {
+            setQuestionsData(partnershipQuestionData);
+        }
+    }, [selectedTopic]);
 
     // Установить первый вопрос как активный при первом рендере
     useEffect(() => {
-        const firstQuestion = questionsData[0];
-        setActiveQuestion(firstQuestion);
-        setIsCommentVisible(true);
-    }, []);
+        if (questionsData.length > 0) {
+            const firstQuestion = questionsData[0];
+            setActiveQuestion(firstQuestion);
+            setIsCommentVisible(true);
+        }
+    }, [questionsData]);
+
     const handleQuestionClick = (question: Question) => {
         if (activeQuestion?.id === question.id) {
-            return
+            return;
         } else {
             setIsCommentVisible(false); // скрываем текущий комментарий
             setTimeout(() => {
@@ -27,8 +49,8 @@ const QuestionsList: React.FC = () => {
     };
 
     return (
-        <>
-            <h1>Экономический блок вопросов</h1>
+        <div className={styles.div}>
+            <h1 className={`${styles.h1}`}>Экономический блок вопросов</h1>
             <div className={styles.container}>
                 {/* Левая часть с вопросами */}
                 <div className={styles.questionsList}>
@@ -50,16 +72,16 @@ const QuestionsList: React.FC = () => {
                     {activeQuestion !== null && (
                         <div className={`${styles.commentContent} ${isCommentVisible ? styles.show : styles.hide}`}>
                             <h2>{activeQuestion && activeQuestion.question}</h2>
-                            <p>{activeQuestion && activeQuestion.comment}</p>
-                            <Link href={'/pages/franchise'}>Back</Link>
+                            <p style={{fontStyle: 'italic'}}>Комментарий эксперта: </p>
 
+                            <p>{activeQuestion && activeQuestion.comment}</p>
+                            <Link className={`${styles.linkBack}`} href={'/pages/franchise'}>Вернуться к выбору тем</Link>
                         </div>
                     )}
                 </div>
             </div>
-        </>
-
+        </div>
     );
 };
 
-export default QuestionsList;
+export default Page;
