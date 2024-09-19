a8bc0639ceac23d62f9a2f3feec0aff992b7488f
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-09-19 15:43:09 +0300

    chore: add loading line animation

--- src/app/pages/business-plan/page.tsx
+++ src/app/pages/business-plan/page.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import styles from './business-plan.module.css'
 
 import Accordion from "@/app/components/accordion/accordion";
+import LoadingLane from "@/app/components/loading-lane/loading-lane";
 const Page = () => {
     return (
         <div className={styles.div} >
+            <LoadingLane/>
             <Accordion />
         </div>
     );
