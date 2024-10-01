44b3a9fd2871ea79aff13e70ba0256641c91d3bd
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-10-01 23:41:17 +0300

    chore: add <meta charSet="UTF-8"/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,9 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
             <ThemeProvider>
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta charSet="UTF-8"/>
                 </Head>
-                <html lang="en">
+                <html lang="ru">
                 <body className={inter.className}>
                 <Header/>
                 {children}
