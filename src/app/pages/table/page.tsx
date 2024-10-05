4a34fcad0ef537e8f7e14fb8bbcd5199c3a2d3e3
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-10-06 01:49:36 +0300

    feat: add drag event;

--- src/app/pages/table/page.tsx
+++ src/app/pages/table/page.tsx
@@ -9,11 +9,15 @@ import styles from './table.module.css';
 const images = [
     { url: 'https://images.unsplash.com/photo-1727086564608-7abb296a677d?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 1', description1: '1 Nulla magna irure incididunt...', description2: '1 Ex irure minim eiusmod...' },
     { url: 'https://images.unsplash.com/photo-1727707185480-a50e6090b58c?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 2', description1: '2 Nulla magna irure incididunt...', description2: '2 Ex irure minim eiusmod...' },
-    { url: 'https://images.unsplash.com/photo-1727717901508-36ae470b697c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 3', description1: '3 Nulla magna irure incididunt...', description2: '3 Ex irure minim eiusmod...' }
+    { url: 'https://images.unsplash.com/photo-1727717901508-36ae470b697c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 3', description1: '3 Nulla magna irure incididunt...', description2: '3 Ex irure minim eiusmod...' },
+    { url: 'https://images.unsplash.com/photo-1727783851864-67d9d2e78ea1?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 4', description1: '4 Nulla magna irure incididunt...', description2: '4 Ex irure minim eiusmod...' },
+    { url: 'https://images.unsplash.com/photo-1727829441330-9a743f524085?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 5', description1: '5 Nulla magna irure incididunt...', description2: '5 Ex irure minim eiusmod...' }
 ];
 
 const Slider: React.FC = () => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [startX, setStartX] = useState<number | null>(null);
+    const [isDragging, setIsDragging] = useState(false);
 
     const handleNext = () => {
         if (activeIndex < images.length - 1) {
@@ -25,6 +29,32 @@ const Slider: React.FC = () => {
         if (activeIndex > 0) {
             setActiveIndex(activeIndex - 1);
         }
+    };
+
+    const handleDragStart = (e: React.MouseEvent | React.TouchEvent) => {
+        const clientX = 'touches' in e ? e.touches[0].clientX : e.clientX;
+        setStartX(clientX);
+        setIsDragging(true);
+    };
+
+    const handleDragMove = (e: React.MouseEvent | React.TouchEvent) => {
+        if (!isDragging || startX === null) return;
+
+        const clientX = 'touches' in e ? e.touches[0].clientX : e.clientX;
+        const deltaX = clientX - startX;
+
+        if (deltaX > 50) {
+            handlePrev();
+            setIsDragging(false); // Остановить перетаскивание после перелистывания
+        } else if (deltaX < -50) {
+            handleNext();
+            setIsDragging(false); // Остановить перетаскивание после перелистывания
+        }
+    };
+
+    const handleDragEnd = () => {
+        setIsDragging(false);
+        setStartX(null);
     };
 
     useEffect(() => {
@@ -44,7 +74,15 @@ const Slider: React.FC = () => {
 
 
     return (
-        <section className={`${styles.slider}`}>
+        <section className={`${styles.slider}`}
+                 onMouseDown={handleDragStart}
+                 onMouseMove={handleDragMove}
+                 onMouseUp={handleDragEnd}
+                 onMouseLeave={handleDragEnd}
+                 onTouchStart={handleDragStart}
+                 onTouchMove={handleDragMove}
+                 onTouchEnd={handleDragEnd}>
+
             <div className={`${styles.list}`}>
                 {images.map((image, index) => (
                     <div
