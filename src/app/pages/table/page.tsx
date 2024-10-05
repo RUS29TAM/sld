1667bf3777a3855f590c999142a4ac65cbff59a3
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-10-06 00:54:15 +0300

    refactor: component links

--- src/app/pages/table/page.tsx
+++ src/app/pages/table/page.tsx
@@ -2,14 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import styles from './table.module.css';
 
-// import img1 from '../../images/img1.jpg';
-// import img2 from '../../images/img2.jpg';
-// import img3 from '../../images/img3.jpg';
+// import img1 from '@/app/images/img1.jpg';
+// import img2 from '@/app/images/img2.jpg';
+// import img3 from '@/app/images/img3.jpg';
 
 const images = [
-    { url: '../../images/img1.jpg', title: 'SLIDER IMAGE MAGIC 1', description1: '1 Nulla magna irure incididunt...', description2: '1 Ex irure minim eiusmod...' },
-    { url: '../../images/img2.jpg', title: 'SLIDER IMAGE MAGIC 2', description1: '2 Nulla magna irure incididunt...', description2: '2 Ex irure minim eiusmod...' },
-    { url: '../../images/img3.jpg', title: 'SLIDER IMAGE MAGIC 3', description1: '3 Nulla magna irure incididunt...', description2: '3 Ex irure minim eiusmod...' }
+    { url: 'https://images.unsplash.com/photo-1727086564608-7abb296a677d?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 1', description1: '1 Nulla magna irure incididunt...', description2: '1 Ex irure minim eiusmod...' },
+    { url: 'https://images.unsplash.com/photo-1727707185480-a50e6090b58c?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 2', description1: '2 Nulla magna irure incididunt...', description2: '2 Ex irure minim eiusmod...' },
+    { url: 'https://images.unsplash.com/photo-1727717901508-36ae470b697c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', title: 'SLIDER IMAGE MAGIC 3', description1: '3 Nulla magna irure incididunt...', description2: '3 Ex irure minim eiusmod...' }
 ];
 
 const Slider: React.FC = () => {
@@ -42,6 +42,7 @@ const Slider: React.FC = () => {
         return () => window.removeEventListener('resize', setDiameter);
     }, []);
 
+
     return (
         <section className={`${styles.slider}`}>
             <div className={`${styles.list}`}>
@@ -49,14 +50,11 @@ const Slider: React.FC = () => {
                     <div
                         key={index}
                         className={`${styles.item} ${index === activeIndex ? styles.active : ''}`}
-                        // style={{ backgroundImage: `url(${image.url})` }}
-                        src={typeof image.url === 'string' ? image.url : String(image.url)}
-
+                        style={{ backgroundImage: `url(${image.url})` }}
                     >
                         <div
                             className={styles.image}
-                            // style={{ backgroundImage: `url(${image.url})` }}
-                            src={typeof image.url === 'string' ? image.url : String(image.url)}
+                            style={{ backgroundImage: `url(${image.url})` }}
                         />
                         <div className={`${styles.content}`}>
                             <h2 className={`${styles.h2}`}>{image.title}</h2>
