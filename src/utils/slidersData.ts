a6a19465bb187cded553c1295d8836d6a6d27938
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-10-15 18:23:10 +0300

    chore: add slidersData

---
+++ src/utils/slidersData.ts
@@ -0,0 +1,88 @@
+import first from "@/app/images/img1.jpg";
+import second from "@/app/images/img2.jpg";
+import third from "@/app/images/img3.jpg";
+import fourth from "@/app/images/img4.jpg";
+import fifth from "@/app/images/img5.jpg";
+
+export const slidesData = [
+    {
+        id: 1,
+        image: first,
+        title: 'DESIGN SLIDER 1',
+        topic: 'Aerphone',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dignissimos error fuga impedit magnam mollitia nulla sint.',
+        detailTitle: 'Aerphone LD',
+        detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        specifications: [
+            { key: 'Used time', value: '6 Hours' },
+            { key: 'Charging port', value: 'Type - C' },
+            { key: 'Compatible', value: 'Android' },
+            { key: 'Bluetooth', value: '5.3' },
+            { key: 'Controlled', value: 'Touch' },
+        ],
+    },
+    {
+        id: 2,
+        image: second,
+        title: 'DESIGN SLIDER 2',
+        topic: 'Headphones',
+        description: 'A new experience in sound.',
+        detailTitle: 'Headphone X2',
+        detailDescription: 'Experience the latest in headphone technology.',
+        specifications: [
+            { key: 'Used time', value: '8 Hours' },
+            { key: 'Charging port', value: 'Micro USB' },
+            { key: 'Compatible', value: 'iOS, Android' },
+            { key: 'Bluetooth', value: '5.1' },
+            { key: 'Controlled', value: 'Button' },
+        ],
+    },
+    {
+        id: 3,
+        image: third,
+        title: 'DESIGN SLIDER 3',
+        topic: 'Aerphone',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dignissimos error fuga impedit magnam mollitia nulla sint.',
+        detailTitle: 'Aerphone LD',
+        detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        specifications: [
+            { key: 'Used time', value: '6 Hours' },
+            { key: 'Charging port', value: 'Type - C' },
+            { key: 'Compatible', value: 'Android' },
+            { key: 'Bluetooth', value: '5.3' },
+            { key: 'Controlled', value: 'Touch' },
+        ],
+    },
+    {
+        id: 4,
+        image: fourth,
+        title: 'DESIGN SLIDER 4',
+        topic: 'Aerphone',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dignissimos error fuga impedit magnam mollitia nulla sint.',
+        detailTitle: 'Aerphone LD',
+        detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        specifications: [
+            { key: 'Used time', value: '6 Hours' },
+            { key: 'Charging port', value: 'Type - C' },
+            { key: 'Compatible', value: 'Android' },
+            { key: 'Bluetooth', value: '5.3' },
+            { key: 'Controlled', value: 'Touch' },
+        ],
+    },
+    {
+        id: 5,
+        image: fifth,
+        title: 'DESIGN SLIDER 5',
+        topic: 'Aerphone',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dignissimos error fuga impedit magnam mollitia nulla sint.',
+        detailTitle: 'Aerphone LD',
+        detailDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        specifications: [
+            { key: 'Used time', value: '6 Hours' },
+            { key: 'Charging port', value: 'Type - C' },
+            { key: 'Compatible', value: 'Android' },
+            { key: 'Bluetooth', value: '5.3' },
+            { key: 'Controlled', value: 'Touch' },
+        ],
+    },
+];
