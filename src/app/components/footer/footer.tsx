23d9a22db3df3d6692a3455805a4611f8ccd7d0a
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-10-18 15:29:35 +0300

    typo: rename file footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from "next/link";
-import styles from "./footet.module.css";
+import styles from "./footer.module.css";
 
 const Footer = () => {
     return (
