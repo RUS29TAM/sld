0a548d16ae11ed2839c7775ce4055ca992f6d289
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-10-21 17:29:19 +0300

    chore: refactor markup

--- src/app/components/frip-card-franchise/flip-card-franchise.tsx
+++ src/app/components/frip-card-franchise/flip-card-franchise.tsx
@@ -17,7 +17,7 @@ const FlipCardFranchise: React.FC = () => {
     const [textContentHeader, setTextContentHeader] = useState<string>('Кликните карточку, чтобы увидеть больше информации.');
     const [textContent, setTextContent] = useState<string>('');
     const [flipSequence, setFlipSequence] = useState<number[]>([]);
-    const { isDarkTheme } = useTheme();
+    const {isDarkTheme} = useTheme();
 
 
     useEffect(() => {
@@ -75,41 +75,45 @@ const FlipCardFranchise: React.FC = () => {
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 {/* Верхний блок */}
-                <div className={`${styles.textBlock} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+                <div className={`${styles.firstBlock} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
                     <h3>Добро пожаловать в раздел, посвященный развитию франчайзинга! Здесь вы найдете информацию
                         необходимую для старта и успешного ведения бизнеса по франшизной модели.</h3>
-                    <div className={`${styles.cardsContainer} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
-                        {cards.map((card, index) => (
-                            <div
-                                key={index}
-                                className={`${styles.card} ${
-                                    flipSequence.includes(index) || activeCardIndex === index ? styles.isFlipped : ''
-                                }`}
-                                onClick={() => handleCardClick(index)}
-                            >
-                                <div className={styles.cardInner}>
-                                    {/* Лицевая часть карточки */}
-                                    <div className={`${styles.cardFront} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
-                                        <p>{card.frontText}</p>
-                                    </div>
-                                    {/* Оборотная сторона карточки */}
-                                    <div className={`${styles.cardBack} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
-                                        <p>{card.backText}</p>
-                                        <a href={card.link} rel="noopener noreferrer">
-                                            Перейти
-                                        </a>
-                                    </div>
+                </div>
+                <div className={`${styles.secondBlock} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+                    {cards.map((card, index) => (
+                        <div
+                            key={index}
+                            className={`${styles.card} ${
+                                flipSequence.includes(index) || activeCardIndex === index ? styles.isFlipped : ''
+                            }`}
+                            onClick={() => handleCardClick(index)}
+                        >
+                            <div className={styles.cardInner}>
+                                {/* Лицевая часть карточки */}
+                                <div
+                                    className={`${styles.cardFront} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+                                    <p>{card.frontText}</p>
+                                </div>
+                                {/* Оборотная сторона карточки */}
+                                <div
+                                    className={`${styles.cardBack} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+                                    <p>{card.backText}</p>
+                                    <a className={`${styles.link}`} href={card.link} rel="noopener noreferrer">
+                                        Перейти
+                                    </a>
                                 </div>
                             </div>
-                        ))}
-                    </div>
-
-                    <div className={`${styles.textContentBox} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
-                        <p>{textContentHeader}</p>
-                        <p>{textContent}</p>
-                    </div>
+                        </div>
+                    ))}
                 </div>
-                <ScrollingAnimation />
+                <div className={`${styles.thirdBlock} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+                    <p>{textContentHeader}</p>
+                    <p>{textContent}</p>
+                </div>
+                <div className={`${styles.fourthBlock} ${isDarkTheme ? styles.darkTheme : styles.lightTheme}`}>
+                    <ScrollingAnimation/>
+                </div>
+
             </div>
         </div>
 
